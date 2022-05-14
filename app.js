1c20df07abd52ab2ddffaefa2883ab8014057ca0
bunchsh <onee.ssong@gmail.com>
2022-05-14 17:10:11 +0900

    submit event와 즉시 실행 함수 preventDefault 적용

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-// function onLoginBtnClick() {
-//     const username = loginInput.value;
-//     if (username === "") {
-//         alert("Please write your name");
-//     } else if (username.length >= 15) {
-//         alert("Your name is too long.");
-//     } else {
-//         alert("Your name is " + username);
-//     }
-// }
+function onLoginSubmit(event) {
+    const username = loginInput.value;
+    event.preventDefault();
+    console.log(username);
+}
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
