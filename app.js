bfb66907cfc73506d03b683375c27a873f951668
bunchsh <onee.ssong@gmail.com>
2022-05-14 15:48:13 +0900

    login form HTML 사용 방법 구현

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    if (username === "") {
-        alert("Please write your name");
-    } else if (username.length >= 15) {
-        alert("Your name is too long.");
-    } else {
-        alert("Your name is " + username);
-    }
-}
+// function onLoginBtnClick() {
+//     const username = loginInput.value;
+//     if (username === "") {
+//         alert("Please write your name");
+//     } else if (username.length >= 15) {
+//         alert("Your name is too long.");
+//     } else {
+//         alert("Your name is " + username);
+//     }
+// }
 
 loginButton.addEventListener("click", onLoginBtnClick);
