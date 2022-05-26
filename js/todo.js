c4d5027924d15ffe7d858f90fa5ee1f3281a160a
bunchsh <onee.ssong@gmail.com>
2022-05-26 16:16:40 +0900

    입력 시, 내용 삭제

--- js/todo.js
+++ js/todo.js
@@ -1,8 +1,12 @@
 const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 function handleTodoList(event) {
     event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    console.log(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleTodoList);
