25db81c53ec416e8b318992e0b7d4d6e19759382
bunchsh <onee.ssong@gmail.com>
2022-05-26 15:55:49 +0900

    요소 상수 선언 및 함수와 이벤트 추가

--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,8 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 
+function handleTodoList(event) {
+    event.preventDefault();
+}
+
+toDoForm.addEventListener("submit", handleTodoList);
