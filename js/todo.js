83b111d75c420c4b47673a8e6870e1f962dedf5e
bunchsh <onee.ssong@gmail.com>
2022-05-26 16:26:52 +0900

    li 와 span 요소 생성 함수 추가

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");;
+    li.appendChild(span);
+    span.innerText = newTodo;
+    console.log(li);
+}
+
 function handleTodoList(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    console.log(newTodo);
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleTodoList);
