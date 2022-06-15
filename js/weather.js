6039985bd54d6a625d8d141cc43b73197ad5325c
bunchsh <onee.ssong@gmail.com>
2022-06-16 02:40:20 +0900

    지역과 날씨 console.log 출력

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,7 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-    fetch(url).then(response => response.json());
+    fetch(url).then(response => response.json().then(data => {console.log(`지역: ${data.name} / 날씨: ${data.weather[0].main}`)}));
 };
 
 function onGeoError() {
