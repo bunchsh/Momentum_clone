e8f8e1921352493d9817a953cade779a7e1abf98
bunchsh <onee.ssong@gmail.com>
2022-06-16 02:28:21 +0900

    섭씨 온도로 변경

--- js/weather.js
+++ js/weather.js
@@ -3,9 +3,9 @@ const API_KEY = "e6fe00af7305bb2de411b2534a05ffbc";
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url);
 };
 
 function onGeoError() {
