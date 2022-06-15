be0033f4ff10c15ad328c38d02d394d9f7392063
bunchsh <onee.ssong@gmail.com>
2022-06-16 02:43:49 +0900

    html 날씨 div 추가 & 지역 및 날씨 변수 설정

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,12 @@ function onGeoOk(position) {
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-    fetch(url).then(response => response.json().then(data => {console.log(`지역: ${data.name} / 날씨: ${data.weather[0].main}`)}));
+    fetch(url).then(response => response.json().then(data => {
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        const name = data.name;
+        const weather = data.weather[0].main;
+    }));
 };
 
 function onGeoError() {
