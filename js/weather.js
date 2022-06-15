2cd2624626944108bc9a13fe96e791b71af41f3b
bunchsh <onee.ssong@gmail.com>
2022-06-16 02:17:22 +0900

    API Key & url 설정

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,11 @@
+const API_KEY = "e6fe00af7305bb2de411b2534a05ffbc";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+    const lon = position.coords.longitude;
     console.log("You live in", lat, lng);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
 };
 
 function onGeoError() {
