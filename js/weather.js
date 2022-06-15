53d946664238c2c91f151d2ef10d5d3c1ce2761b
bunchsh <onee.ssong@gmail.com>
2022-06-16 01:32:52 +0900

    콘솔 테스트

--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,9 @@
-function onGeoOk() {
-
+function onGeoOk(position) {
+    console.log(position);
 };
 
 function onGeoError() {
-
+    alert("Can't find you, No weather for you.")
 };
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
